d7c73f1ca534ecb88347a39ad026b0d8c5354836
Julia <julia.zhmudyk@gmail.com>
2023-09-11 14:51:30 +0200

    updated ui

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <p className=" text-inactive-text pt-40 pl-0 md:pl-52 pb-10 text-center">
+      <p className=" text-inactive-text pt-10 md:pt-40 pl-0 md:pl-52 pb-10 text-center">
         This prosuct uses the TMBb API is not endorsed or certified by TMDb
       </p>
     </footer>
